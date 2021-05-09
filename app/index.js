789e7280519e419d1b7a1e48a44a6b718f2ddc71
elham-salehi <elham.s69@gmail.com>
2021-05-10 02:37:03 +0430

    connect database

---
+++ app/index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+const ErrorMiddleware = require('./http/middleware/Error');
+const app = express();
+
+class Application {
+    constructor() {
+        this.setupExpressServer();
+        this.setupMongoose();
+        this.setupRoutesAndMiddlewares();
+        this.setupConfigs();
+    }
+
+    setupRoutesAndMiddlewares() {
+        // built-in middleware
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+        app.use(express.static('public'));
+
+        if (app.get('env') === 'production') app.use(morgan('tiny'));
+
+        // third-party middleware
+        app.use(cors());
+
+        //routes
+        /// ...
+
+        app.use(ErrorMiddleware);
+    }
+
+    setupConfigs() {
+        winston.add(new winston.transports.File({ filename: 'error-log.log' }));
+        winston.add(
+            new winston.transports.MongoDB({
+                db: 'mongodb://localhost:27017/divar',
+                level: 'error',
+            }),
+        );
+
+        process.on('uncaughtException', (err) => {
+            console.log(err);
+            winston.error(err.message);
+        });
+        process.on('unhandledRejection', (err) => {
+            console.log(err);
+            winston.error(err.message);
+        });
+
+        app.set('view engine', 'pug');
+        app.set('views', '../views'); // default
+    }
+
+    setupMongoose() {
+        mongoose
+            .connect('mongodb://localhost:27017/divar', {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
+            .then(() => {
+                console.log('db connected');
+                winston.info('db connected');
+            })
+            .catch((err) => {
+                console.error('db not connected', err);
+            });
+    }
+    setupExpressServer() {
+        const port = process.env.myPort || 3000;
+        app.listen(port, (err) => {
+            if (err) console.log(err);
+            else console.log(`app listen to port ${port}`);
+        });
+    }
+}
+
+module.exports = Application;
